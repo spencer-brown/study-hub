154226bd68ac7ac853dc987e9a9f55118d6981d4
Spencer Brown <me@spencer.sx>
2013-12-19 15:28:23 -0500

    implement devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Studypool::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Devise --> change to actual host
+  config.action_mailer.default_url_options = { :host => 'studypool.herokuapps.com' }
+
 end
