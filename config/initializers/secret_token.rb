d9e08fe1e9caadfc890abf084eb364acbcd7ff59
Spencer Brown <me@spencer.sx>
2013-12-19 14:41:55 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Studypool::Application.config.secret_key_base = '25dc10dc04a83b4d636019561517f964bca13e6e9b23308b72a7d5d6e0af6db953fd997269954ebef7cafeca9685d5166bc453610ea8f09d7c459e0e77aacb82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Studypool::Application.config.session_store :cookie_store, key: '_studypool_session'
