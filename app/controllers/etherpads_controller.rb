bd7e78361b4e1a4a03920974f28454b0397ad06f
Spencer Brown <me@spencer.sx>
2013-12-21 00:16:42 -0500

    comment stuff that won't work on heroku

--- app/controllers/etherpads_controller.rb
+++ app/controllers/etherpads_controller.rb
@@ -30,14 +30,16 @@ class EtherpadsController < ApplicationController
     # --------------------------------------
 
     # using the lower-level client allows me to use the api methods from the EPL docs
-    client = ether.client
+    # client = ether.client
 
-    client.getText(padID: "kirby")
-    client.setText(padID: "my_first_etherpad_lite_pad", text: "asdfasdfO")
-    client.getText(padID: "my_first_etherpad_lite_pad")
-    client.setText(padID: "testpad2", text: "this is testpad2")
-    client.getText(padID: "testpad2")
-    client.listAllPads()
+    # COMMENT TEMPORARILY FOR HEROKU PUSH
+    # --------------------------------------
+    # client.getText(padID: "kirby")
+    # client.setText(padID: "my_first_etherpad_lite_pad", text: "asdfasdfO")
+    # client.getText(padID: "my_first_etherpad_lite_pad")
+    # client.setText(padID: "testpad2", text: "this is testpad2")
+    # client.getText(padID: "testpad2")
+    # client.listAllPads()
 
 
   end
