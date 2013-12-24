a992a5e1744874c83ecf3a527ca0055e36eea54a
Spencer Brown <me@spencer.sx>
2013-12-24 01:31:32 -0500

    new button that begins with Apply or Remove depending on whether the user is in a group already

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,6 +10,7 @@ class GroupsController < ApplicationController
   # GET /groups/1
   # GET /groups/1.json
   def show
+    @determine = current_user.groups.include? @group
   end
 
   # GET /groups/new
