95b2e5e8e2ee52127c917ae99ae6ea69a76590c5
Spencer Brown <me@spencer.sx>
2013-12-29 17:02:40 -0500

    require @purdue.edu email address when signing up, add placeholders to sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :email_regexp => /.+@purdue.edu\z/i
+
 
   has_and_belongs_to_many :groups, -> { uniq }
   has_many :etherpads, :through => :groups
