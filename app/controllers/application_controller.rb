dfb3d3dc31d7da4cc5ddcf8a330cb3b5e8130012
Spencer Brown <me@spencer.sx>
2014-01-09 01:11:53 -0500

    change devise sign in path to be the dashboard, sign out path to be the homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,14 @@ class ApplicationController < ActionController::Base
 
 	protected
 
+	def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    dashboard_path
+  end
+
 	def configure_permitted_parameters
 	  devise_parameter_sanitizer.for(:sign_up) << :name
 	  devise_parameter_sanitizer.for(:account_update) << :name
