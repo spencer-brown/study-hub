3b5e711978161c736dbc8e1af10b2c61d1a226ab
Spencer Brown <me@spencer.sx>
2014-01-27 01:16:36 -0500

    intialize development environment variables

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Load the app's custom environment variables here, so that they are loaded before environments/*.rb
+app_environment_variables = File.join(Rails.root, 'config', 'env.rb')
+load(app_environment_variables) if File.exists?(app_environment_variables)
+
 # Initialize the Rails application.
 Studypool::Application.initialize!
