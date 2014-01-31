973d5df62bbe8176fd973783e62818a3194a0c17
Spencer Brown <me@spencer.sx>
2014-01-30 21:36:51 -0500

    big update, new homepage bg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,15 @@ class ApplicationController < ActionController::Base
 
 	protected
 
-	def after_sign_out_path_for(resource_or_scope)
+	def after_sign_out_path_for(user)
     root_path
   end
 
-  def after_sign_in_path_for(resource_or_scope)
+  def after_sign_in_path_for(user)
+    dashboard_path
+  end
+
+  def after_sign_up_path_for(user)
     dashboard_path
   end
 
