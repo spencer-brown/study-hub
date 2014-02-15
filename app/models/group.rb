165d48144c4dba2211a36dcc1dbda6ec31838126
Spencer Brown <me@spencer.sx>
2014-02-15 18:06:32 -0500

    remove searchkick/elasticsearch

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,8 +2,6 @@ class Group < ActiveRecord::Base
   has_and_belongs_to_many :users, -> { uniq }
   has_many :etherpads
   belongs_to :subject
-  
-  searchkick
 
   def groupname
     "#{group.name}"
